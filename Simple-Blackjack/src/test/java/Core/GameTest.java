78e6883f2949058054378e5c46c7bec63235b338
korede97 <korede97@users.noreply.github.com>
2018-09-09 19:45:19 -0400

    Commented features that needed to be test

--- Simple-Blackjack/src/test/java/Core/GameTest.java
+++ Simple-Blackjack/src/test/java/Core/GameTest.java
@@ -4,7 +4,27 @@ import junit.framework.TestCase;
 
 public class GameTest extends TestCase {
 	
-	public void drawCard() {
+	public void drawCardTest() {
+		
 		
 	}
+	
+	//input mode test
+	
+	//player turn 
+	
+	//draw 2 cards 
+		//2 each for player and dealer
+		//check card visibility 
+			//both visible for player 
+			//one/two visible for the dealer 
+	
+	//player gets to continuously choose hit or stand. Ended by stand 
+		//if stand then their turn ends 
+	
+	
+	//enforce dealer rules 
+	
+	//isGameWinner test
+	
 }
