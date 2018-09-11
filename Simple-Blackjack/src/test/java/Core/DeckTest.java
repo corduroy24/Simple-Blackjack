4297d3cf25bc55caa25939ee0d760af495c8a81e
korede97 <korede97@users.noreply.github.com>
2018-09-11 14:57:55 -0400

    failed all tests in deckTest class

--- Simple-Blackjack/src/test/java/Core/DeckTest.java
+++ Simple-Blackjack/src/test/java/Core/DeckTest.java
@@ -25,17 +25,53 @@ public class DeckTest extends TestCase{
 	     deck.addCard(card_4); 
 	     
 	     assertEquals(false, deck.isEmpty()); 
+	     
+
+	     assertEquals(card_1,deck.get(1)); 
+
+	}
+	
+	public void addCardsTest() {
+	     List<Card> deck = new ArrayList<Card>();
+	     List <Card> cardsToAdd = new ArrayList<Card>();
+	     
+	     Card card_1 = new Card("1","Spades"); 
+	     Card card_2 = new Card("1","Spades"); 
+	     Card card_3 = new Card("1","Spades"); 
+	     Card card_4 = new Card("1","Spades"); 
+	     
+	     deck.cardsToAdd(card_1); 
+	     deck.cardsToAdd(card_2); 
+	     deck.cardsToAdd(card_3); 
+	     deck.cardsToAdd(card_4); 
+	     
+	     deck.addCardsTest(cardsToAdd); 
+	     
+	     assertEquals(false, deck.isEmpty()); 
 
 	     assertEquals(card_1,deck.get(1)); 
 
 	}
 	
 	public void drawCardTest() {
-	    /* List<Card> deck = new ArrayList<Card>();
+	     List<Card> deck = new ArrayList<Card>();
+	     Card cardToRemove = deck.get(deck.size()-1);  
+	     
+	     int deckSizeBefore; 
+	     int deckSizeAfter; 
+	     boolean removedCard = false; 
+	     
+	     deckSizeBefore = deck.size(); 
 	     for(int i = 0; i < deckSize;i++ ) {
-	    	 deck.add
-	     }*/
-	    
+	    	 deck.remove(deck.size()-1); 
+	    	 removedCard = true; 
+	     }
+	     deckSizeAfter = deck.size(); 
+	     
+	     assertEquals((deckSizeBefore - 1), deckSizeAfter);
+	     
+	     assertTrue(removedCard); 
+	        
 	}
     
 
