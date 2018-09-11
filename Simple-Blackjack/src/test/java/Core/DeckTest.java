0e19f3d86978cbe096db218ea5b74f41197ccf78
korede97 <korede97@users.noreply.github.com>
2018-09-11 10:38:52 -0400

    Aligned style to TDD - created add card test to deckTest class

--- Simple-Blackjack/src/test/java/Core/DeckTest.java
+++ Simple-Blackjack/src/test/java/Core/DeckTest.java
@@ -7,8 +7,36 @@ import java.util.Collections;
 import junit.framework.TestCase; 
 
 
+
 public class DeckTest extends TestCase{
 	
+	static int deckSize = 4; 
+	
+	public void addCardTest() {
+	     List<Card> deck = new ArrayList<Card>();
+	     Card card_1 = new Card("1","Spades"); 
+	     Card card_2 = new Card("1","Spades"); 
+	     Card card_3 = new Card("1","Spades"); 
+	     Card card_4 = new Card("1","Spades"); 
+	     
+	     deck.addCard(card_1); 
+	     deck.addCard(card_2); 
+	     deck.addCard(card_3); 
+	     deck.addCard(card_4); 
+	     
+	     assertEquals(false, deck.isEmpty()); 
+
+	     assertEquals(card_1,deck.get(1)); 
+
+	}
+	
+	public void drawCardTest() {
+	    /* List<Card> deck = new ArrayList<Card>();
+	     for(int i = 0; i < deckSize;i++ ) {
+	    	 deck.add
+	     }*/
+	    
+	}
     
 
 
