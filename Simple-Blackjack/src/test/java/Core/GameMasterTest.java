1ab1a017a9f131d99bb7f03cf68c21108ca9d904
korede97 <korede97@users.noreply.github.com>
2018-09-19 11:48:04 -0400

    completed tests for 24 and 29

--- Simple-Blackjack/src/test/java/Core/GameMasterTest.java
+++ Simple-Blackjack/src/test/java/Core/GameMasterTest.java
@@ -4,26 +4,37 @@ import junit.framework.TestCase;
 
 public class GameMasterTest extends TestCase {
 	
-	/*public void testEndOfTurn() {
+	public void testEndOfTurn() {
 		GameMaster game = new GameMaster();
-		String [] input = { "CA", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "CJ", "CQ", "CK",
-				"DA", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10", "DJ", "DQ", "DK",
-				"HA", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "HJ", "HQ", "HK",
-				"SA", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10", "SJ", "SQ", "SK"};
+
 		
-	//	Deck shuffleDeck = new Deck();
-		Player player = new Player(); 
-		game.deck.createDefaultDeck(input);
-		game.deck.shuffleDeck();
-		char inputH = 'H'; 
-		char inputS = 'S'; 
+		Card card1 = new Card("S4"); 
+		Card card2 = new Card("D6"); 
+		Card card3 = new Card("S8"); 
+		Card card4 = new Card("SJ"); 
+		Card card5 = new Card("S7"); 
+		Card card6 = new Card("S3"); 
+
 		
-		game.bettingSequence(inputH);
-		game.bettingSequence(inputS);
 		
-		assertFalse(game.player.getHand().turn);
+
+		game.player.getHand().addCard(card1);
+		game.player.getHand().addCard(card2);
 		
-	}*/
+		game.dealer.getHand().addCard(card3);
+		game.dealer.getHand().addCard(card4);
+		
+		game.deck.addCard(card5);
+		game.deck.addCard(card6);
+		
+		game.player.getHand().turn = true; 
+		game.bettingSequence('H');
+		game.bettingSequence('S');
+		assertFalse(game.player.getHand().turn); 
+		
+		System.out.println("----------");
+
+	}
 	
 	public void testPlayerBustDealerWins() {
 		GameMaster game = new GameMaster();
